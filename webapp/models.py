8911682f105ae1398f984b9ea84adf099fa52ddf
Alex Merk <durango77@gmail.com>
2021-12-24 22:01:41 +0300

    обновленный каркас

--- webapp/models.py
+++ webapp/models.py
@@ -6,17 +6,6 @@ class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(80), unique=True)
     email = db.Column(db.String(120), unique=True)
-    Phone_number = db.Column(db.String(11), unique=True)
-    Password = db.Column(db.String(30), unique=False)
 
     def __repr__(self):
         return '<User %r>' % self.username
-
-class Post(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    body = db.Column(db.String(140))
-    timestamp = db.Column(db.DateTime, index=True, default=datetime.utcnow)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
-
-    def __repr__(self):
-        return '<Post {}>'.format(self.body)
--- webapp/routes.py
+++ webapp/routes.py
@@ -1,13 +1,12 @@
 from flask import render_template, flash, redirect, url_for
 from webapp import app
-from webapp.forms import LoginForm
 
 
 @app.route('/')
 @app.route('/index')
 def index():
     user = {'username': 'Alex'}
-    posts = [
+    post = [
         {
             'author': {'username': 'John'},
             'body': 'Beautiful day in Portland!'
@@ -17,14 +16,6 @@ def index():
             'body': 'The Avengers movie was so cool!'
         }
     ]
-    return render_template('index.html', title='Home', user=user, posts=posts)
+    return render_template('index.html', title='Home', user=user, post=post)
 
 
-@app.route('/login', methods=['GET', 'POST'])
-def login():
-    form = LoginForm()
-    if form.validate_on_submit():
-        flash('Login requested for user {}, remember_me={}'.format(
-            form.username.data, form.remember_me.data))
-        return redirect(url_for('index'))
-    return render_template('login.html',  title='Sign In', form=form)
