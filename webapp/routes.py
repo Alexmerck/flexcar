aa1935b5f22bf9ed53f1225b9551ebb0f876b714
Shestakov Aleksandr <santellla@mail.ru>
2021-12-30 20:48:27 +0300

    made saving in db from carinput html

--- webapp/routes.py
+++ webapp/routes.py
@@ -5,6 +5,9 @@ from config import Config
 from flask_login import LoginManager, login_user, logout_user
 from webapp.models import User 
 from webapp.forms import CarinputForm
+from webapp import db
+from webapp.models import Vehicle
+
 
 
 @app.route('/')
@@ -37,6 +40,8 @@ def process_login():
 def logout():
     logout_user()
     return redirect(url_for('index'))
+
+
 @app.route('/usercars')
 def usercar():
     user = {'username': 'testuser'}
@@ -56,3 +61,24 @@ def сarinput():
     title = "Добавление авто в гараж"
     сarinput_form = CarinputForm()
     return render_template('carinput.html', page_title=title, form=сarinput_form)
+
+
+@app.route('/carinput', methods=["POST"])
+def process_сarinput():
+    form = CarinputForm()
+    if form.validate_on_submit():
+        car =  Vehicle(
+            title=form.title.data, 
+            manufacturer=form.manufacturer.data, 
+            model=form.model.data, 
+            production_year=form.production_year.data,
+            engine_type=form.engine_type.data,
+            volume=form.volume.data,
+            transmission_type=form.transmission_type.data,
+            body=form.body.data
+            )
+        db.session.add(car)
+        db.session.commit()
+        
+        return redirect(url_for('usercar'))
+
