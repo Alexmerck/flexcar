955cbaf1cb50b37a9ab77a5bd226be6cd1d62621
Alex Merk <durango77@gmail.com>
2021-12-30 10:21:28 +0300

    обновил форму логирования

--- webapp/models.py
+++ webapp/models.py
@@ -1,16 +1,22 @@
 from datetime import datetime
 from enum import unique
 from webapp import db
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
 
-
-class User(db.Model):
+class User(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key = True)
     username = db.Column(db.String(80), unique = True)
     email = db.Column(db.String(120), unique = True)
     phone_number = db.Column(db.Integer, unique = True)
-    password = db.Column(db.String(30), unique = False)
+    password = db.Column(db.String(130), unique = False)
     events = db.relationship('Event', backref='author', lazy='dynamic')
     vehicles = db.relationship('Vehicle', backref='Name', lazy='dynamic')
+    def set_password(self, password):
+        self.password = generate_password_hash(password)
+    def check_password(self, password):
+        return check_password_hash(self.password, password)
+
 
 class Event(db.Model):
     id = db.Column(db.Integer, primary_key = True)
@@ -18,7 +24,7 @@ class Event(db.Model):
     published = db.Column(db.DateTime, index=True, default=datetime.utcnow)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
     charges = db.Column(db.Integer)
-    vehicle_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    vehicle_id = db.Column(db.Integer, db.ForeignKey('vehicle.id'))
 
 
 class Vehicle(db.Model):
@@ -32,7 +38,7 @@ class Vehicle(db.Model):
     volume = db.Column(db.Integer)
     transmission_type = db.Column(db.Integer)
     body = db.Column(db.Integer)
-    events = db.relationship('Event', backref='author', lazy='dynamic')
+    events = db.relationship('Event', backref='name', lazy='dynamic')
 
     def __repr__(self):
         return '<User %r>' % self.username
--- webapp/routes.py
+++ webapp/routes.py
@@ -1,21 +1,37 @@
 from flask import render_template, flash, redirect, url_for
 from webapp import app
-
+from webapp.forms import LoginForm
+from config import Config
+from flask_login import LoginManager, login_user, logout_user
+from webapp.models import User 
 
 @app.route('/')
 @app.route('/index')
 def index():
     user = {'username': 'Alex'}
-    post = [
-        {
-            'author': {'username': 'John'},
-            'body': 'Beautiful day in Portland!'
-        },
-        {
-            'author': {'username': 'Susan'},
-            'body': 'The Avengers movie was so cool!'
-        }
-    ]
-    return render_template('index.html', title='Home', user=user, post=post)
+    return render_template('index.html', title='Home', user=user)
+
+@app.route('/login')
+def login():
+    title = "Авторизация"
+    login_form = LoginForm()
+    return render_template('login.html', page_title=title, form=login_form)
 
 
+@app.route('/process-login', methods=['POST'])
+def process_login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        user = User.query.filter_by(username=form.username.data).first()
+        if user and user.check_password(form.password.data):
+            login_user(user)
+            flash('Вы вошли на сайт')
+            return redirect(url_for('index'))
+    flash('Неправильное имя пользователя или пароль')
+    return redirect(url_for('login'))
+
+
+@app.route('/logout')
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
